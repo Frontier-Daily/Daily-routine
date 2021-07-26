0084382d27160b45874e8e4cce5e8f9970935bca
yeonlisa <tkzw4869@gmail.com>
2021-07-26 20:55:36 +0900

    yeonlisa's stack&queue

--- part-1/src/queue.js
+++ part-1/src/queue.js
@@ -5,11 +5,23 @@ class Queue {
     this.rear = 0;
   }
 
-  size() {}
+  size() {
+    return Object.keys(this.storage).length;
+  }
 
-  enqueue(element) {}
+  enqueue(element) {
+    return this.storage[this.rear++] = element;
+  }
 
-  dequeue() {}
+  dequeue() {
+    let current = this.storage[this.front];
+
+    delete this.storage[this.front];
+    if(this.front < this.rear) {
+      this.front++;
+    }
+    return current;
+  }
 }
 
 module.exports = Queue;
--- part-1/src/stack.js
+++ part-1/src/stack.js
@@ -4,11 +4,23 @@ class Stack {
     this.top = -1;
   }
 
-  size() {}
+  size() {
+    return Object.keys(this.storage).length;
+  }
 
-  push(element) {}
+  push(element) {
+    return this.storage[++this.top] = element;
+  }
 
-  pop() {}
+  pop() {
+    let current = this.storage[this.top];
+    
+    if(this.top < 0) {
+      return undefined;
+    }
+    delete this.storage[this.top--];
+    return current;
+  }
 }
 
 module.exports = Stack;
